e9c84b059f55d12b4d2b67f979ef8f96e06584f5
Amanda-msv <amandamsveiga@gmail.com>
2025-12-01 16:53:13 -0300

    .

---
+++ Aula 4 - Arrays/Matriz/src/exercicio/InicializacaoMatriz.java
@@ -0,0 +1,17 @@
+package exercicio;
+
+import java.util.Arrays;
+
+public class InicializacaoMatriz {
+    public static void main(String[] args) {
+        String[] nomes = {"Diego", "Fernando", "José"};
+
+        System.out.println(Arrays.toString(nomes));
+
+        System.out.println("");
+
+
+
+
+    }
+}
