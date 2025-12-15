937bf28396371645f292e79ea4c97647e0749f6a
Amanda-msv <amandamsveiga@gmail.com>
2025-12-15 17:03:37 -0300

    .

---
+++ Aula 8 - CRUD/CrudAlunos/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.example.entidades.Aluno;
+import org.example.entidades.GerenciarAlunos;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        GerenciarAlunos gerenciarAlunos = new GerenciarAlunos();
+
+        Aluno aluno1 = new Aluno("Diego",10,5);
+        gerenciarAlunos.cadastrarAluno(aluno1);
+        gerenciarAlunos.excluirAluno(1);
+
+    }
+
+}
---
+++ Aula 8 - CRUD/CrudAlunos/src/main/java/org/example/entidades/GerenciarAlunos.java
@@ -0,0 +1,58 @@
+package org.example.entidades;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GerenciarAlunos {
+
+    private final String url = "jdbc:mysql://localhost:3306/escola";
+    private final String usuario = "root";
+    private final String senha = "admin";
+
+    //Metodo para conectar no banco de dados
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(url, usuario, senha);
+    }
+
+    // Cadastrar Aluno
+    public void cadastrarAluno( Aluno aluno){
+
+        String sql = "INSERT INTO alunos (nome, nota, nota2)" + "VALUES (?, ?, ?)";
+
+        try {
+            Connection conexao = conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, aluno.getNome());
+            stmt.setDouble(2, aluno.getNota());
+            stmt.setDouble(3, aluno.getNota());
+
+            stmt.executeUpdate();
+
+            System.out.println("Aluno cadastrado com sucesso!");
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void excluirAluno (int id){
+        String sql = "DELETE FROM alunos WHERE id = ?";
+
+        try {
+            Connection conexao = conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.executeUpdate();
+            System.out.println("Aluno excluido com sucesso!");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
+
